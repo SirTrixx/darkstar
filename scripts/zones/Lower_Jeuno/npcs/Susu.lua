ca4ef779622872b0873c90a73bc91f696e91eac3
Matt Kauper <mhk4g@virginia.edu>
2016-10-06 21:30:54 -0400

    Give Susu back here missing scrolls

--- scripts/zones/Lower_Jeuno/npcs/Susu.lua
+++ scripts/zones/Lower_Jeuno/npcs/Susu.lua
@@ -13,17 +13,29 @@ require("scripts/zones/Lower_Jeuno/TextIDs");
 -----------------------------------
 
 function onTrade(player,npc,trade)
-end; 
+end;
 
 -----------------------------------
 -- onTrigger Action
 -----------------------------------
 
 function onTrigger(player,npc)
-    
+
 player:showText(npc,SUSU_SHOP_DIALOG);
 
-stock = {0x1227,20000,        -- Banishga II
+stock = {0x12BE,12000,      -- Stonega II
+     0x12C8,13050,      -- Waterga II
+     0x12B9,19800,      -- Aeroga II
+     0x12AF,21307,      -- Firaga II
+     0x12B4,26244,      -- Blizzaga II
+     0x12C3,31681,      -- Thundaga II
+     0x12CC,71250,      -- Flare
+     0x12CE,52500,      -- Freeze
+     0x12D0,56250,      -- Tornado
+     0x12D2,60000,      -- Quake
+     0x12D4,63750,      -- Burst
+     0x12D6,67500,      -- Flood
+     0x1227,20000,        -- Banishga II
      0x1248,244,        -- Barsleep
      0x1249,400,        -- Barpoison
      0x124a,780,        -- Barparalyze
@@ -38,10 +50,11 @@ stock = {0x1227,20000,        -- Banishga II
      0x1215,35000,        -- Holy
      0x1211,2330,        -- Silena
      0x1212,19200,        -- Stona
-     0x1213,13300}        -- Viruna
- 
+     0x1213,13300,      -- Viruna
+ }
+
 showShop(player, STATIC, stock);
-end; 
+end;
 
 -----------------------------------
 -- onEventUpdate
@@ -60,6 +73,3 @@ function onEventFinish(player,csid,option)
 --printf("CSID: %u",csid);
 --printf("RESULT: %u",option);
 end;
-
-
-
